8e1abb233bc586ce178cd208f1cea19ebcafedf4
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2023-03-28 20:51:56 +0200

    create posts page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace DesignPatterns_SocialMedia.Controllers
             return View();
         }
 
+        public IActionResult Posts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
