efb1267391667c604b65c9b0d6166c56ebb24cb0
Stefan Untura <u_stefan@mail.com>
2023-03-29 14:50:45 +0200

    Implement http post and storing input data in CreatePost controller

---
+++ Controllers/CreatePostController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns_SocialMedia.Controllers
+{
+    public class CreatePostController : Controller
+    {
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost(string title, string content)
+        {
+            try
+            {
+                Console.WriteLine("trying");
+                ViewData["postTitle"] = Request.Form["postTitle"].First();
+                ViewData["postContent"] = Request.Form["postContent"].First();
+
+                return View();
+            }
+            catch
+            {
+                Console.WriteLine("error");
+                return View();
+            }
+        }
+    }
+}
