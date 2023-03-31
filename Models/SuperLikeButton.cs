eb11dfdabea67d84d28caf64b1c8700ce507e5c6
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2023-03-31 14:48:58 +0200

    observer buttons
    >with subscriber interface
    >abstract button class

---
+++ Models/SuperLikeButton.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns_SocialMedia.Models
+{
+    public class SuperLikeButton : Button
+    {
+        public override void Click()
+        {
+            NotifySubscribers();
+        }
+    }
+}
