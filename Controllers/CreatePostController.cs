e9c659aff772942fe02b0c034eb598120b6d3245
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2023-03-31 12:21:26 +0200

    fix viewing and creating posts

--- Controllers/CreatePostController.cs
+++ Controllers/CreatePostController.cs
@@ -6,6 +6,15 @@ namespace DesignPatterns_SocialMedia.Controllers
 {
     public class CreatePostController : Controller
     {
+        private readonly PostCreator textPostCreator = new TextPostCreator();
+        private readonly PostCreator imagePostCreator = new ImagePostCreator();
+        private readonly PostCreator videoPostCreator = new VideoPostCreator();
+        private Post? post;
+
+        private string? postTitle;
+        private string? postContent;
+        private string? postType;
+        
         public IActionResult CreatePost()
         {
             return View();
@@ -20,11 +29,31 @@ namespace DesignPatterns_SocialMedia.Controllers
                 ViewData["postContent"] = Request.Form["postContent"].First();
                 ViewData["postType"] = Request.Form["postType"].First();
 
-                TempData["postTitle"] = Request.Form["postTitle"].First();
-                TempData["postContent"] = Request.Form["postContent"].First();
-                TempData["postType"] = Request.Form["postType"].First();
+                postType = Request.Form["postType"].First() as String;
+                postTitle = Request.Form["postTitle"].First() as String;
+                postContent = Request.Form["postContent"].First() as String;
 
-
+                switch (postType)
+                {
+                    case "Text":
+                        post = CreateTextPost(CreatedPosts.GetID(), postContent, postTitle);
+                        CreatedPosts.IncreaseID();
+                        CreatedPosts.AddPost(post);
+                        break;
+                    case "Image":
+                        post = CreateImagePost(CreatedPosts.GetID(), postContent, postTitle);
+                        CreatedPosts.IncreaseID();
+                        CreatedPosts.AddPost(post);
+                        break;
+                    case "Video":
+                        post = CreateVideoPost(CreatedPosts.GetID(), postContent, postTitle);
+                        CreatedPosts.IncreaseID();
+                        CreatedPosts.AddPost(post);
+                        break;
+                    default:
+                        return RedirectToAction("Index", "Home");
+                        break;
+                }
                 return RedirectToAction("Posts", "Posts");
             }
             catch
@@ -32,6 +61,23 @@ namespace DesignPatterns_SocialMedia.Controllers
                 return View();
             }
         }
+
+        private Post CreateTextPost(int id, String content, String title)
+        {
+            return textPostCreator.CreatePost(id, content, title);
+
+        }
+
+        private Post CreateImagePost(int id, String content, String title)
+        {
+            return imagePostCreator.CreatePost(id, content, title);
+        }
+
+        private Post CreateVideoPost(int id, String content, String title)
+        {
+            return videoPostCreator.CreatePost(id, content, title);
+        }
+
 
     }
 }
--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -8,74 +8,70 @@ namespace DesignPatterns_SocialMedia.Controllers
     public class PostsController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly PostCreator textPostCreator = new TextPostCreator();
-        private readonly PostCreator imagePostCreator = new ImagePostCreator();
-        private readonly PostCreator videoPostCreator = new VideoPostCreator();
-        private Post post, post1, post2, post3, post4;
-        private List<Post> posts;
-        private int id = 5;
+        //private readonly PostCreator textPostCreator = new TextPostCreator();
+        //private readonly PostCreator imagePostCreator = new ImagePostCreator();
+        //private readonly PostCreator videoPostCreator = new VideoPostCreator();
+        //private Post post, post1, post2, post3, post4;
+        //private List<Post> posts;
+        //private int id = 5;
 
 
-        string? postTitle;
-        string? postContent;
-        string? postType;
+        //string? postTitle;
+        //string? postContent;
+        //string? postType;
 
         public PostsController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            posts = new List<Post>();
-            post1 = CreateTextPost(1, "This is the content", "This is the title");
-            post2 = CreateTextPost(2, "This is the content again", "Text Post");
-            post3 = CreateImagePost(3, "https://i.redd.it/5gnz9r0fcaqa1.jpg", "Image Post");
-            post4 = CreateVideoPost(4, "https://vimeo.com/810735707", "Video Post");
-            posts.Add(post1);
-            posts.Add(post2);
-            posts.Add(post3);
-            posts.Add(post4);
         }
-        private Post CreateTextPost(int id, String content, String title)
-        {
-            return textPostCreator.CreatePost(id, content, title);
+        //private Post CreateTextPost(int id, String content, String title)
+        //{
+        //    return textPostCreator.CreatePost(id, content, title);
         
-        }
+        //}
 
-        private Post CreateImagePost(int id, String content, String title)
-        {
-            return imagePostCreator.CreatePost(id, content, title);
-        }
+        //private Post CreateImagePost(int id, String content, String title)
+        //{
+        //    return imagePostCreator.CreatePost(id, content, title);
+        //}
 
-        private Post CreateVideoPost(int id, String content, String title)
-        {
-            return videoPostCreator.CreatePost(id, content, title);
-        }
+        //private Post CreateVideoPost(int id, String content, String title)
+        //{
+        //    return videoPostCreator.CreatePost(id, content, title);
+        //}
 
         public IActionResult Posts()
         {
-            postType = TempData["postType"] as String;
-            postTitle = TempData["postTitle"] as String;
-            postContent = TempData["postContent"] as String;
-
-            switch (postType)
-            {
-                case "Text":
-                    post = CreateTextPost(id, postContent, postTitle);
-                    id++;
-                    break;
-                case "Image":
-                    post = CreateImagePost(id, postContent, postTitle);
-                    id++;
-                    break;
-                case "Video":
-                    post = CreateVideoPost(id, postContent, postTitle);
-                    id++;
-                    break;
-                default:
-                    break;
-            }
-            posts.Add(post);
-            
-            return View(posts);
+            return View(CreatedPosts.GetPosts());
         }
+
+        //public IActionResult CreatedPost()
+        //{
+        //    postType = TempData["postType"] as String;
+        //    postTitle = TempData["postTitle"] as String;
+        //    postContent = TempData["postContent"] as String;
+
+        //    switch (postType)
+        //    {
+        //        case "Text":
+        //            post = CreateTextPost(id, postContent, postTitle);
+        //            id++;
+        //            break;
+        //        case "Image":
+        //            post = CreateImagePost(id, postContent, postTitle);
+        //            id++;
+        //            break;
+        //        case "Video":
+        //            post = CreateVideoPost(id, postContent, postTitle);
+        //            id++;
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //    posts.Add(post);
+
+        //    return View(posts);
+        //}
 
     }
 }
