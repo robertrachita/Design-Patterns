e0453860bdedcf5788e966af12b525de27a2f4a7
Stefan Untura <u_stefan@mail.com>
2023-03-31 18:45:58 +0200

    Observer pattern functionality

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -6,9 +6,11 @@ using Microsoft.Extensions.Hosting;
 
 namespace DesignPatterns_SocialMedia.Controllers
 {
-    public class PostsController : Controller
+    public class PostsController : Controller, Subscriber
     {
         private readonly ILogger<HomeController> _logger;
+
+        private EditButton editButton = new EditButton();
 
         public PostsController(ILogger<HomeController> logger)
         {
@@ -30,11 +32,14 @@ namespace DesignPatterns_SocialMedia.Controllers
         [HttpPost]
         public ActionResult ViewPost(int id, int value)
         {
+            editButton.AddSubscriber(this);
+
             Post post = getPostById(id);
             LikeDecorator like = new LikeDecorator();
             post.AddLike(like);
             post.AddComment(value.ToString());
 
+            editButton.NotifySubscribers();
 
             return RedirectToAction("Posts", "Posts");
         }
@@ -49,6 +54,11 @@ namespace DesignPatterns_SocialMedia.Controllers
                 }
             }
             return null;
+        }
+
+        public void Update()
+        {
+            JavaScriptHandler.javaScriptAlert = "ALERT: Post edited successfuly!";
         }
     }
 }
